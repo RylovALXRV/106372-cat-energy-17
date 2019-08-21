88f9241ceff1b0ac2e3ce63fbf25ee0aa831f69d
Алексей <rylovalex2108l@mail.ru>
2019-08-21 16:31:03 +0700

    исправил ошибки, поработал с графикой.

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,36 @@ var sourcemap = require("gulp-sourcemaps");
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
 var server = require("browser-sync").create();
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/sprite/*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"))
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 92}))
+    .pipe(gulp.dest("source/img"))
+});
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
