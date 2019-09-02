a7b3af89fd14647fb1494601db6b3f509b275ca2
Алексей <rylovalex2108l@mail.ru>
2019-09-02 19:15:21 +0700

    Добавил статичную карту без js

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,11 @@
 "use strict";
+
 var navigationElement = document.querySelector(".navigation");
 var navigationBtnElement = navigationElement.querySelector(".navigation__btn");
+var addressBlockElement = document.querySelector(".address-block");
 
 navigationElement.classList.remove("navigation--nojs");
+addressBlockElement.classList.remove("address-block--nojs");
 
 navigationBtnElement.addEventListener("click", function () {
   if (navigationElement.classList.contains("navigation--closed")) {
